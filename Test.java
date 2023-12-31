cb135334886a2f28b680d01501a64774952ba384
Jayarpj <155310386+Jayarpj@users.noreply.github.com>
2023-12-31 21:35:30 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -4,6 +4,7 @@ class Test
     {
         System.out.println("My First Java Program.");
 		System.out.println("Hello Siva Prasad.");
+	        System.out.println("Hello 2024.");
 	        System.out.println("Hello Staragile.");
     }
 };
